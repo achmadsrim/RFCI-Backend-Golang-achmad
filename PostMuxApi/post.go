67816810a13b079ddd70da12067f98b3ed66da2e
xiyyu <achmad.srim@gmail.com>
2020-11-19 19:06:05 +0700

    Perubahan JSON

--- PostMuxApi/post.go
+++ PostMuxApi/post.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 type Post struct {
 	Author   string `json:"author"`
+	Title    string `json:"title"`
 	Comments []struct {
 		Message string `json:"message"`
 	} `json:"comments"`
-	Title string `json:"title"`
 }
 
 var posts []Post
@@ -27,14 +25,13 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var post Post
 	_ = json.NewDecoder(r.Body).Decode(&post)
-	post.ID = strconv.Itoa(rand.Intn(1000000))
 	posts = append(posts, post)
 	json.NewEncoder(w).Encode(&post)
 }
 
 func main() {
 	router := mux.NewRouter()
-	posts = append(posts, Post{Author: "Who", Comments: "This is the content of my first post", Title: "My first post"})
+	posts = append(posts, Post{Author: "Who", Title: "My first post"})
 	router.HandleFunc("/posts", getPosts).Methods("GET")
 	router.HandleFunc("/posts", createPost).Methods("POST")
 	http.ListenAndServe(":8000", router)
